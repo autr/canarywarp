fbb622bfe5fcdead293dce5a1931a7e2797ed737
GSinnott <g@sinnott.cc>
2021-03-25 19:36:36 +0100

    build

---
+++ precache-manifest.86e0efd1ec922bbd92c491e82239c910.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdeb2f68de4bfcd7d71c",
+    "url": "/static/css/main.61ce593c.chunk.css"
+  },
+  {
+    "revision": "bdeb2f68de4bfcd7d71c",
+    "url": "/static/js/main.ff7f7429.chunk.js"
+  },
+  {
+    "revision": "0b83c393278e4e5a8fda",
+    "url": "/static/js/runtime~main.40766258.js"
+  },
+  {
+    "revision": "45dfbcefd680bac2ad92",
+    "url": "/static/js/2.1d3de4ec.chunk.js"
+  },
+  {
+    "revision": "ec0899cc179994cd1963e26c34c06d26",
+    "url": "/index.html"
+  }
+];
