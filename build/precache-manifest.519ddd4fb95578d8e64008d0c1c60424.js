f5cda40a184cd63320458345e842a8e3633e9136
GSinnott <g@sinnott.cc>
2021-03-25 21:04:21 +0100

    rebuild w/ gh-pages

---
+++ build/precache-manifest.519ddd4fb95578d8e64008d0c1c60424.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bc390e71bda9369328e",
+    "url": "/canarywarp/static/css/main.61ce593c.chunk.css"
+  },
+  {
+    "revision": "1bc390e71bda9369328e",
+    "url": "/canarywarp/static/js/main.acf31ddc.chunk.js"
+  },
+  {
+    "revision": "3578be7ccf047f7eca40",
+    "url": "/canarywarp/static/js/runtime~main.17c4fd50.js"
+  },
+  {
+    "revision": "5c5f454335d8bbd2b424",
+    "url": "/canarywarp/static/js/2.dad1a328.chunk.js"
+  },
+  {
+    "revision": "d12e7bb723bc967a5c02ea3de86ae4d6",
+    "url": "/canarywarp/index.html"
+  }
+];
