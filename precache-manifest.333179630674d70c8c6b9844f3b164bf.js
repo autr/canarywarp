c822000677547dabebe77bbe5ac80c675aa7fee7
GSinnott <g@sinnott.cc>
2021-04-13 12:45:54 +0200

    Updates

--- precache-manifest.333179630674d70c8c6b9844f3b164bf.js
+++ precache-manifest.333179630674d70c8c6b9844f3b164bf.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "32a5227ddae1c60579ac",
-    "url": "/canarywarp/static/css/main.61ce593c.chunk.css"
+    "revision": "944ad4e101a96382f296",
+    "url": "/canarywarp/static/css/main.993e7964.chunk.css"
   },
   {
-    "revision": "32a5227ddae1c60579ac",
-    "url": "/canarywarp/static/js/main.3a27f929.chunk.js"
+    "revision": "944ad4e101a96382f296",
+    "url": "/canarywarp/static/js/main.4a59c8cf.chunk.js"
   },
   {
     "revision": "3578be7ccf047f7eca40",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/canarywarp/static/js/2.dad1a328.chunk.js"
   },
   {
-    "revision": "941f9c84a01793cf2e3bf6942af658b1",
+    "revision": "156fab6ddddd655d00e45b08b301582a",
     "url": "/canarywarp/index.html"
   }
 ];
